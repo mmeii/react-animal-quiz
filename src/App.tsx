aa1d4ddae9056a0237c063b8764edce513751660
Mengmei Tu <redmmeii@gmail.com>
2021-03-22 17:44:36 -0500

    setup structures in app, questioncard, and basic fetch api

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { fetchQuizQuestions } from './API';
+//components
+import QuestionCard from './components/QuestionCard';
+//types
+import { Difficulty } from './API';
 
-function App() {
+const TOTAL_QUESTIONS = 10;
+
+const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState([]);
+  const [score, setScore] = useState(0);
+  const [gameover, setGameover] = useState(true);
+
+  // call API to fetch
+  console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY));
+
+  const startQuiz = async () => {
+
+  };
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+
+  };
+
+  const nextQuestion = () => {
+
+  };
+
   return (
     <div className="App">
-      Quiz
+      <h1>ANIMALS QUIZ</h1>
+
+      <button
+        className="start"
+        onClick={startQuiz}
+      >
+        Start
+      </button>
+
+      <p className="score">Score:</p>
+      <p>Loading Questions ...</p>
+
+      {/* <QuestionCard
+        questionNum={number + 1}
+        totalQuestions={TOTAL_QUESTIONS}
+        question={questions[number].question}
+        answers={questions[number].answers}
+        userAnswer={userAnswers ? userAnswers[number] : undefined}
+        callback={checkAnswer}
+      /> */}
+
+      <button
+        className="start"
+        onClick={nextQuestion}
+      >
+        Next Question
+      </button>
     </div>
   );
 }
